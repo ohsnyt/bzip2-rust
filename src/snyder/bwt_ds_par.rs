88746f8c04f40a2cd76e61b14c7b66a2c3273c90
Oh Snyt <61324584+ohsnyt@users.noreply.github.com>
2022-12-06 09:22:31 -0600

    Speed improvements to bwt_ds_par

--- src/snyder/bwt_ds_par.rs
+++ src/snyder/bwt_ds_par.rs
@@ -1,7 +1,10 @@
 use core::cmp::Ordering;
-use log::info;
+use log::{debug, error, info, warn};
 use rayon::prelude::*;
-use std::mem;
+use std::{mem, sync::Arc};
+
+const SORT_XPOINT: usize = 40000; //Approx point at which parallel work is faster than sequential for sorting
+const VEC_XPOINT: usize = 75000; //Approx point at which parallel work is faster for BwtKey Vec creation
 
 /// Struct for Burrows-Wheeler-Transform data.
 /// Contains the index to the original data order, a multi-byte sort
@@ -43,53 +46,75 @@ impl PartialEq for BwtKey {
     }
 }
 
-/// Parallel BTW sorting algorithm. ds. 2022.
+/// Parallel bwt sorting algorithm. ds. 2022.
 /// ENTRY POINT
 pub fn bwt_encode_par(data: &[u8]) -> (u32, Vec<u8>) {
-    info!("Entering parallel BTW sorting algorithm.");
+    let now = std::time::Instant::now();
+    info!("Entering parallel bwt sorting algorithm.");
 
     // Create usize sorting values
     let udata: Vec<usize> = udata_par_map(data);
     // Create vec of custom structs for sorting
     let mut bwt_data = convert_to_bwt_data(data, &udata);
     // Do smart initial sort of the data
-    par_bwt_sort(&mut bwt_data);
+    let now = std::time::Instant::now();
+    if bwt_data.len() > SORT_XPOINT {
+        bwt_data.par_sort();
+    } else {
+        bwt_data.sort();
+    }
+    debug!("Initial sort took {:?}", now.elapsed());
 
     // Repeatedly sort the data as long as we find identical sequences in it.
     let mut sub_depth = 1;
-    while par_subsorting(&mut bwt_data, sub_depth, &udata) {
-        info!("Run depth is {}", sub_depth);
+    while subsorting(&mut bwt_data, sub_depth, &udata) {
         sub_depth += 1;
     }
     // return the vec of sorted data
-    match find_key(&bwt_data) {
-        Some(key) => {
-            return (
+    // Logic for parallel vs sequential
+
+    let end = bwt_data.len();
+    if end > SORT_XPOINT {
+        /// Return key entry from bwt_keys
+        if let Some(key) = bwt_data
+            .par_iter()
+            .enumerate()
+            .find_first(|(_, el)| el.index == 0)
+            .map(|(i, _)| i as u32)
+        {
+            (
                 key,
                 bwt_data.par_iter().map(|el| el.symbol).collect::<Vec<u8>>(),
             )
+        } else {
+            (0, vec![])
         }
-        None => (0, vec![]),
-    }
-}
-
-/// Smart-ish BWT sort function, invokes parallel sorting for larger data sets.
-fn par_bwt_sort(data: &mut [BwtKey]) {
-    // Don't spool up threads if only a few items to sort
-    if data.len() > 15 {
-        data.par_sort()
     } else {
-        data.sort()
+        if let Some(key) = bwt_data
+            .iter()
+            .enumerate()
+            .find(|(_, el)| el.index == 0)
+            .map(|(i, _)| i as u32)
+        {
+            (
+                key,
+                bwt_data.iter().map(|el| el.symbol).collect::<Vec<u8>>(),
+            )
+        } else {
+            (0, vec![])
+        }
     }
 }
 
 /// Parallel update BwtKey data after sort
-fn par_subsorting(data: &mut [BwtKey], rundepth: u32, udata: &Vec<usize>) -> bool {
+fn subsorting(data: &mut [BwtKey], rundepth: u32, udata: &Vec<usize>) -> bool {
+    let now = std::time::Instant::now();
     // Create tuples of all identical sort key sequences
     let mut seqs: Vec<(usize, usize)> = Vec::new();
     // Limit local variables to this block
     {
-        // Local variable looks for runs
+        // Local variable looks for runs. Done sequentially because we don't want to
+        // accidentally split data in the middle of a run.
         let mut run = 1_usize;
         for i in 1..data.len() {
             if data[i - 1] == data[i] {
@@ -109,54 +134,79 @@ fn par_subsorting(data: &mut [BwtKey], rundepth: u32, udata: &Vec<usize>) -> boo
     }
 
     // Otherwise update the keys and sort the sequences
-    //seqs.iter().for_each(|(start, end)| {
-    //});
     seqs.iter().for_each(|(start, end)| {
-        update_btw_keys(&mut data[*start..*end], rundepth, udata);
-        data[*start..*end].par_sort();
+        update_bwt_keys(&mut data[*start..*end], rundepth, udata);
+        if end - start > SORT_XPOINT {
+            data[*start..*end].par_sort();
+            debug!("{} par keys. ", end - start);
+        } else {
+            data[*start..*end].sort();
+        }
     });
+
+    debug!("Run depth {} took {:?}.", rundepth, now.elapsed());
 
     // Return true (we sorted something)
     true
 }
 
 /// Convert data to BwtKey vector
-fn convert_to_bwt_data(data: &[u8], udata: &[usize]) -> Vec<BwtKey> {
+fn convert_to_bwt_data(mut data: &[u8], udata: &[usize]) -> Vec<BwtKey> {
     let end = data.len();
-    data.par_iter()
-        .enumerate()
-        .map(|(i, _)| {
-            BwtKey::new(
-                ((end - i) % end) as u32,
-                udata[(end - i) % end],
-                data[(end - 1) - i % end],
-            )
-        })
-        .collect::<Vec<BwtKey>>()
+    if end > SORT_XPOINT {
+        data.par_iter()
+            .enumerate()
+            .map(|(i, _)| {
+                BwtKey::new(
+                    ((end - i) % end) as u32,
+                    udata[(end - i) % end],
+                    data[(end - 1) - i % end],
+                )
+            })
+            .collect::<Vec<BwtKey>>()
+    } else {
+        data.iter()
+            .enumerate()
+            .map(|(i, _)| {
+                BwtKey::new(
+                    ((end - i) % end) as u32,
+                    udata[(end - i) % end],
+                    data[(end - 1) - i % end],
+                )
+            })
+            .collect::<Vec<BwtKey>>()
+    }
 }
 
-/// Update btw_keys to next depth level
-fn update_btw_keys(data: &mut [BwtKey], depth: u32, udata: &Vec<usize>) {
+/// Update bwt_keys to next depth level
+fn update_bwt_keys(mut data: &mut [BwtKey], depth: u32, udata: &Vec<usize>) {
     let end = udata.len();
-    data.iter_mut().for_each(|el| {
-        el.sort = udata[(el.index as usize + (std::mem::size_of::<usize>()) * depth as usize) % end]
-            as usize;
-        el.depth = depth as u16;
-    });
-}
-
-/// Return key entry from btw_keys
-fn find_key(data: &[BwtKey]) -> Option<u32> {
-    data.par_iter()
-        .enumerate()
-        .find_first(|(_, el)| el.index == 0)
-        .map(|(i, _)| i as u32)
+    // Parallel processing helps with performance when the data set is over 75k in size.
+    if end > VEC_XPOINT {
+        pub fn convert_a2(mut data: &[u8], udata: &[usize]) -> Vec<BwtKey> {
+            let end = data.len();
+            data.par_iter()
+                .enumerate()
+                .map(|(i, sym)| BwtKey {
+                    index: ((end - i) % end) as u32,
+                    sort: udata[(end - i) % end],
+                    depth: 0,
+                    symbol: data[(end - i) % end],
+                })
+                .collect()
+        }
+    }
+    let offset = (std::mem::size_of::<usize>()) * (depth as usize);
+    for i in 0..data.len() {
+        data[i].sort = udata[(data[i].index as usize + offset) % end];
+        data[i].depth = depth as u16;
+    }
 }
 
 /// Create fast usize sorting data from input.
 /// Combines multiple input u8s into one usize.
 /// Currently designed for 64, 32, 16 and 8 bit system architectures.
-fn udata_par_map(data: &[u8]) -> Vec<usize> {
+pub fn udata_par_map(data: &[u8]) -> Vec<usize> {
     // Get the OS memory size
     let size = mem::size_of::<usize>();
 
@@ -169,65 +219,133 @@ fn udata_par_map(data: &[u8]) -> Vec<usize> {
         extend -= (extend).min(data.len());
     }
 
-    // Match on usize length (in bytes)
-    match size {
-        /* When Rust goes to a 128 bit usize, add this code
-        16 => ext_data
-        .par_windows(size)
-        .map(|w| {
-            (w[0] as usize) << 120
-            | (w[1] as usize) << 112
-            | (w[2] as usize) << 104
-            | (w[3] as usize) << 96
-            | (w[4] as usize) << 88
-            | (w[5] as usize) << 80
-            | (w[6] as usize) << 72
-            | (w[7] as usize) << 64
-            | (w[8] as usize) << 56
-            | (w[9] as usize) << 48
-            | (w[10] as usize) << 40
-            | (w[11] as usize) << 32
-            | (w[12] as usize) << 24
-            | (w[13] as usize) << 16
-            | (w[14] as usize) << 8
-            | (w[15] as usize)
-        })
-        .collect(),
-        */
-        8 => ext_data
+    // Logic to choose parallel vs sequential iteration
+    let end = ext_data.len();
+    if end > SORT_XPOINT {
+        // Match on usize length (in bytes)
+        match size {
+            /* When Rust goes to a 128 bit usize, add this code
+            16 => ext_data
             .par_windows(size)
-            .enumerate()
-            .map(|(_, w)| {
-                (w[0] as usize) << 56
-                    | (w[1] as usize) << 48
-                    | (w[2] as usize) << 40
-                    | (w[3] as usize) << 32
-                    | (w[4] as usize) << 24
-                    | (w[5] as usize) << 16
-                    | (w[6] as usize) << 8
-                    | (w[7] as usize)
+            .map(|w| {
+                (w[0] as usize) << 120
+                | (w[1] as usize) << 112
+                | (w[2] as usize) << 104
+                | (w[3] as usize) << 96
+                | (w[4] as usize) << 88
+                | (w[5] as usize) << 80
+                | (w[6] as usize) << 72
+                | (w[7] as usize) << 64
+                | (w[8] as usize) << 56
+                | (w[9] as usize) << 48
+                | (w[10] as usize) << 40
+                | (w[11] as usize) << 32
+                | (w[12] as usize) << 24
+                | (w[13] as usize) << 16
+                | (w[14] as usize) << 8
+                | (w[15] as usize)
             })
             .collect(),
-        4 => ext_data
-            .par_windows(size)
-            .enumerate()
-            .map(|(_, w)| {
-                (w[0] as usize) << 24
-                    | (w[1] as usize) << 16
-                    | (w[2] as usize) << 8
-                    | (w[3] as usize)
+            */
+            8 => ext_data
+                .par_windows(size)
+                .enumerate()
+                .map(|(_, w)| {
+                    (w[0] as usize) << 56
+                        | (w[1] as usize) << 48
+                        | (w[2] as usize) << 40
+                        | (w[3] as usize) << 32
+                        | (w[4] as usize) << 24
+                        | (w[5] as usize) << 16
+                        | (w[6] as usize) << 8
+                        | (w[7] as usize)
+                })
+                .collect(),
+            4 => ext_data
+                .par_windows(size)
+                .enumerate()
+                .map(|(_, w)| {
+                    (w[0] as usize) << 24
+                        | (w[1] as usize) << 16
+                        | (w[2] as usize) << 8
+                        | (w[3] as usize)
+                })
+                .collect(),
+            2 => ext_data
+                .par_windows(size)
+                .enumerate()
+                .map(|(_, w)| (w[0] as usize) << 8 | (w[1] as usize))
+                .collect(),
+            1 => ext_data.par_iter().map(|b| *b as usize).collect(),
+            _ =>
+            // Unplanned OS architecture.
+            {
+                error!("Unplanned OS architecture. Aborting.");
+                panic!()
+            }
+        }
+    } else {
+        // Match on usize length (in bytes)
+        match size {
+            /* When Rust goes to a 128 bit usize, add this code
+            16 => ext_data
+            .windows(size)
+            .map(|w| {
+                (w[0] as usize) << 120
+                | (w[1] as usize) << 112
+                | (w[2] as usize) << 104
+                | (w[3] as usize) << 96
+                | (w[4] as usize) << 88
+                | (w[5] as usize) << 80
+                | (w[6] as usize) << 72
+                | (w[7] as usize) << 64
+                | (w[8] as usize) << 56
+                | (w[9] as usize) << 48
+                | (w[10] as usize) << 40
+                | (w[11] as usize) << 32
+                | (w[12] as usize) << 24
+                | (w[13] as usize) << 16
+                | (w[14] as usize) << 8
+                | (w[15] as usize)
             })
             .collect(),
-        2 => ext_data
-            .par_windows(size)
-            .enumerate()
-            .map(|(_, w)| (w[0] as usize) << 8 | (w[1] as usize))
-            .collect(),
-        1 => ext_data.par_iter().map(|b| *b as usize).collect(),
-        _ =>
-        // Unplanned OS architecture - possibly 8 bit system.
-        {
-            panic!()
+            */
+            8 => ext_data
+                .windows(size)
+                .enumerate()
+                .map(|(_, w)| {
+                    (w[0] as usize) << 56
+                        | (w[1] as usize) << 48
+                        | (w[2] as usize) << 40
+                        | (w[3] as usize) << 32
+                        | (w[4] as usize) << 24
+                        | (w[5] as usize) << 16
+                        | (w[6] as usize) << 8
+                        | (w[7] as usize)
+                })
+                .collect(),
+            4 => ext_data
+                .windows(size)
+                .enumerate()
+                .map(|(_, w)| {
+                    (w[0] as usize) << 24
+                        | (w[1] as usize) << 16
+                        | (w[2] as usize) << 8
+                        | (w[3] as usize)
+                })
+                .collect(),
+            2 => ext_data
+                .windows(size)
+                .enumerate()
+                .map(|(_, w)| (w[0] as usize) << 8 | (w[1] as usize))
+                .collect(),
+            1 => ext_data.iter().map(|b| *b as usize).collect(),
+            _ =>
+            // Unplanned OS architecture - possibly 8 bit system.
+            {
+                error!("Unplanned OS architecture. Aborting.");
+                panic!()
+            }
         }
     }
 }
--- src/unused/report.rs
+++
@@ -1,9 +0,0 @@
-use super::options::{BzOpts, Verbosity};
-
-/// Reports app progress as per Verbosity setting. 
-/// Accepts both &str and String message.
-pub fn report<S: AsRef<str> + std::fmt::Display>(o: &BzOpts, this_v: Verbosity, msg: S) {
-    if o.verbosity >= this_v {
-        println!("{}", msg)
-    }
-}

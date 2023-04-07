78853cbc99846752d85dc0424f30fcb4abd5a174
Oh Snyt <61324584+ohsnyt@users.noreply.github.com>
2023-04-06 20:19:16 -0500

    Converted freq_out to array (from vec)

--- src/tools/freq_count.rs
+++ src/tools/freq_count.rs
@@ -1,31 +1,30 @@
-//! Frequency counting for the Rust version of the standard BZIP2 library.
+//! Optimized byte frequency counting for a slice of u8 data.
 //!
-//! Create a vec of 256 u32 integers which hold the frequency counts of each byte found in the block of 
+//! Create an array of 256 u32 integers which hold the frequency counts of each byte found in the block of 
 //! data given to the freqs function. 
 //! 
-//! This will use multi-threading when the data is over 16k in length.
+//! NOTE: This will use multi-threading when the data is over 16k in length.
 //!
 
 use rayon::prelude::*;
 
 /// Returns a frequency count of the input data. 
-pub fn freqs(data: &[u8]) -> Vec<u32> {
+pub fn freqs(data: &[u8]) -> [u32;256] {
     if data.len() > 64_000 {
         // 16k is pretty much the sweet spot for chunk size.
         data.par_chunks(16_000)
             .fold(
-                || vec![0_u32; 256],
-                |mut freqs, chunk| {
+                || [0_u32; 256],
+                |mut freqs:[u32;256], chunk| {
                     chunk.iter().for_each(|&el| freqs[el as usize] += 1);
                     freqs
                 },
             )
             .reduce(
-                || vec![0_u32; 256],
-                |s, f| s.iter().zip(&f).map(|(a, b)| a + b).collect::<Vec<u32>>(),
-            )
+                || [0_u32; 256],
+                |s, f| {let x = s.iter().zip(&f).map(|(a, b)| a + b); let mut z = [0_u32; 256]; for (i, el) in x.enumerate() {z[i] = el}; z})
     } else {
-        let mut freqs = vec![0_u32; 256];
+        let mut freqs = [0_u32; 256];
         data.iter().for_each(|&el| freqs[el as usize] += 1);
         freqs
     }
